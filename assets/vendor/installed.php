186f7802dcb6548f50c43b3bda85898789a8b995
Yassine Gharsallah <MohamedYassine.Gharsallah@esprit.tn>
2025-02-05 20:53:28 +0100

    .gititgnore changed

---
+++ assets/vendor/installed.php
@@ -0,0 +1,216 @@
+<?php return array (
+  '@hotwired/stimulus' => 
+  array (
+    'version' => '3.2.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'react' => 
+  array (
+    'version' => '18.3.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'react-dom' => 
+  array (
+    'version' => '18.3.1',
+    'dependencies' => 
+    array (
+      0 => 'react',
+      1 => 'scheduler',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'scheduler' => 
+  array (
+    'version' => '0.23.2',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/fade-theme' => 
+  array (
+    'version' => '1.0.5',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/slide-theme' => 
+  array (
+    'version' => '1.0.5',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/forms-plugin' => 
+  array (
+    'version' => '2.0.1',
+    'dependencies' => 
+    array (
+      0 => '@swup/plugin',
+      1 => 'swup',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/plugin' => 
+  array (
+    'version' => '2.0.3',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'swup' => 
+  array (
+    'version' => '3.1.1',
+    'dependencies' => 
+    array (
+      0 => 'delegate-it',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'delegate-it' => 
+  array (
+    'version' => '6.0.1',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@swup/debug-plugin' => 
+  array (
+    'version' => '3.0.0',
+    'dependencies' => 
+    array (
+      0 => '@swup/plugin',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@hotwired/turbo' => 
+  array (
+    'version' => '7.3.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'vue' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+      0 => '@vue/runtime-dom',
+      1 => '@vue/runtime-dom',
+      2 => '@vue/runtime-dom',
+      3 => '@vue/compiler-dom',
+      4 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/runtime-dom' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+      0 => '@vue/runtime-core',
+      1 => '@vue/runtime-core',
+      2 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/compiler-dom' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+      0 => '@vue/compiler-core',
+      1 => '@vue/compiler-core',
+      2 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/shared' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/runtime-core' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+      0 => '@vue/reactivity',
+      1 => '@vue/reactivity',
+      2 => '@vue/shared',
+      3 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/compiler-core' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+      0 => '@vue/shared',
+      1 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  '@vue/reactivity' => 
+  array (
+    'version' => '3.5.13',
+    'dependencies' => 
+    array (
+      0 => '@vue/shared',
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
