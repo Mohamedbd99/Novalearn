2fcb3ef0427f5f3199daaef0ae96a67935def9a0
Yassine Gharsallah <MohamedYassine.Gharsallah@esprit.tn>
2025-02-10 21:43:28 +0100

    Ajout des fichiers ignorés

---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/home' => [[['_route' => 'app_home', '_controller' => 'App\\Controller\\DefaultController::index'], null, null, null, false, false, null]],
+        '/signup' => [[['_route' => 'app_signup', '_controller' => 'App\\Controller\\SignupController::signup'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+    ],
+    [ // $dynamicRoutes
+    ],
+    null, // $checkCondition
+];
